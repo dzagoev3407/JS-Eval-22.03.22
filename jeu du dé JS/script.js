7bfbd53211e1347cc3493036121e395503ce50f6
Bonnefoy Kévin <93328879+dzagoev3407@users.noreply.github.com>
2022-03-29 18:18:28 +0200

    MAJ du 29.03.22 à 18h18

--- jeu du dé JS/script.js
+++ jeu du dé JS/script.js
@@ -24,7 +24,7 @@ let namePlayer = 'Player 1'
 
 /* Stylisation du compteur */
 
-$(compteurOne).css('color', '#dc828c')
+$(compteurOne).css('color', '#FF5C0F')
 $(compteurOne).css('font-size', '34px')
 $(compteurOne).css('margin-left', '340px')
 
@@ -39,7 +39,7 @@ btnAdd.addEventListener('click', function comptage() {
          document.write(textCompteurOne.innerHTML = ('Nombre de points de ' + namePlayer + ' est ' + compteurOne + ' pts.'))
         } // Affiche le nombre de points du Player 1
 
-    if(compteurTwo == 9) {
+    if(compteurOne == 9) {
         console.log('Player 1 a gagné la partie !')
     }else {
         console.log('Player 1 a perdu la partie !')
@@ -57,7 +57,7 @@ let namePlayerTwo = 'Player 2'
 
 /* Stylisation du compteur */
 
-$(compteurTwo).css('color', '#dc828c')
+$(compteurTwo).css('color', '#0017FF')
 $(compteurTwo).css('font-size', '34px')
 $(compteurTwo).css('margin-left', '178px')
 
@@ -68,6 +68,7 @@ btnAddTWO.addEventListener('click', function comptage() {
         let textCompteurOne = ('Nombre de points de ' + namePlayerTwo + ' est ' + compteurTwo + ' pts.')
         document.write(textCompteurOne.innerHTML = ('Nombre de points de ' + namePlayerTwo + ' est ' + compteurTwo + ' pts.'))
         } // 
+
     if(compteurTwo == 9) {
         console.log('Player 2 a gagné la partie !')
     }else {
@@ -105,17 +106,17 @@ btnAddTWO.addEventListener('click', function comptage() {
       listResultats.appendChild(ul);
   
       let face1 = document.createElement("img");
-      face1.setAttribute("src","https://cdn.pixabay.com/photo/2013/07/12/17/39/dice-152173_960_720.png");
+      face1.setAttribute("src","https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Dice-1-b.svg/1024px-Dice-1-b.svg.png");
       let face2 = document.createElement("img");
-      face2.setAttribute("src","https://cdn.pixabay.com/photo/2013/07/12/17/39/dice-152174_960_720.png");
+      face2.setAttribute("src","https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Dice-2-b.svg/2048px-Dice-2-b.svg.png");
       let face3 = document.createElement("img");
-      face3.setAttribute("src","https://cdn.pixabay.com/photo/2013/07/12/17/39/dice-152175_960_720.png");
+      face3.setAttribute("src","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuOeH6XZg5q5A93s6IfqRYCPxYi9g8ugO78Y6kn1Gmo8AMc4gTrgLCNY2oY9bPE8nt5R8&usqp=CAU");
       let face4 = document.createElement("img");
-      face4.setAttribute("src","https://cdn.pixabay.com/photo/2013/07/12/17/39/dice-152176_960_720.png");
+      face4.setAttribute("src","https://img1.freepng.fr/20180315/hpq/kisspng-dice-dominoes-bunco-clip-art-dice-face-5aaad1951c1d49.1818678715211442131152.jpg");
       let face5 = document.createElement("img");
-      face5.setAttribute("src","https://cdn.pixabay.com/photo/2013/07/12/17/39/dice-152177_960_720.png");
+      face5.setAttribute("src","https://upload.wikimedia.org/wikipedia/commons/0/08/Dice-5-b.svg");
       let face6 = document.createElement("img");
-      face6.setAttribute("src","https://cdn.pixabay.com/photo/2013/07/12/17/39/dice-152178_960_720.png");
+      face6.setAttribute("src","https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Dice-6-b.svg/1024px-Dice-6-b.svg.png");
       let newItem = document.createElement("li");
   
       switch (resultat) {
@@ -161,3 +162,18 @@ btnAddTWO.addEventListener('click', function comptage() {
     $(face5).css('width', '50px').css('heigth', '50px').css('ul', 'none')
     $(face6).css('width', '50px').css('heigth', '50px').css('ul', 'none')
   }
+
+  /* BTN scroll vers le haut */
+
+  let btnScroll = document.querySelector('.scrollBtn')
+
+  btnScroll.addEventListener('click', () => {
+      window.scrollTo({
+          top: 100,
+          left: 100,
+          behavior: 'smooth'
+      })
+  })
+
+  /*========================================================
+     ======================================================= */
